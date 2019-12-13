71eecf4db22466dfe94474d88ac0a0a6cf916b3e
Ferran Tomas <ferrandinand@users.noreply.github.com>
2019-12-13 11:44:51 +0100

    Improve example

--- examples/simple-example/main.tf
+++ examples/simple-example/main.tf
@@ -2,34 +2,33 @@ provider "giantswarm" {}
 
 # Using AWS example, remove Azure resource
 resource "giantswarm_cluster" "test" {
-  owner = "giantswarm" 
-  name = "Ferran est_cluster" 
+  owner = "giantswarm"
+  name = "Ferran est_cluster"
   release_version= "8.6.0"
   availability_zones = 1
-  
-  workers_min = 2 
-  workers_max = 3 
 
-  worker_aws_ec2_instance_type = "m5.large" 
+  workers_min = 2
+  workers_max = 3
+
+  worker_aws_ec2_instance_type = "m5.large"
   count = 0
 }
 resource "giantswarm_app" "test_app"{
-      cluster_id = "${giantswarm_cluster.test_azure.id}"
-  		app_name = "kong-app"
-			catalog = "giantswarm-incubator"
-			name = "kong-app" 
-			namespace = "kong"
-			version = "0.2.0" 
-      count = 0
+  cluster_id = "${giantswarm_cluster.test_azure.id}"
+  app_name = "kong-app"
+  catalog = "giantswarm-incubator"
+  name = "kong-app"
+  namespace = "kong"
+  version = "0.4.5"
+  count = 0
 }
 
 # Using Azure example, remove AWS resource
 resource "giantswarm_cluster" "test_azure" {
   owner = "giantswarm"
   name = "Ferran tf test_cluster"
-  release_version= "8.5.0"
-  workers_min = 3
-  workers_max = 3
+  release_version= "9.0.0"
+  workers_min = 3 # Giantswarm azore has no autoscaler yet.
+  workers_max = 3 # workers min/max should be the same.
   worker_azure_vm_size = "Standard_D2s_v3"
-  count = 1
 }
